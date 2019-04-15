ce2bf612c3d659481a197f12171fde17a506e256
tobiasvanegten <tvanegten@hotmail.com>
2019-04-15 16:00:32 +0200

    headers

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,9 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 // User resource
 Route::get('/users', 'UserController@index');
