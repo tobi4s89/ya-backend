f0db0e4071d21db0e3fc555c0baee4827e140cc9
tobiasvanegten <tvanegten@hotmail.com>
2019-04-15 15:03:03 +0200

    added barryvdh to kernel middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class
+        \App\Http\Middleware\TrustProxies::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -40,9 +41,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            'cors' =>[
-                \Barryvdh\Cors\HandleCors::class,
-            ]
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
