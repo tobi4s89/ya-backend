5d1b9550860fc47e1036d9c904ad5796782cd424
tobiasvanegten <tvanegten@hotmail.com>
2019-04-15 16:14:29 +0200

    reset barryvdh package

--- app/User.php
+++ app/User.php
@@ -10,7 +10,7 @@ class User extends Model
 
     protected $fillable = ['name'];
 
-    protected $hidden = ['created_at', 'deleted_at', 'updated_at'];
+    protected $hidden = ['created_at', 'updated_at'];
 
     public function properties()
     {
